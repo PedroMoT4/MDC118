4d69d30b87c092efba9f2d9e45a2842c7b530401
Pedro <pedroheriquecarvalhomota@gmail.com>
2020-11-04 12:14:07 -0300

    tempo.c

--- Atividades/Atividade01/tempo.c
+++ Atividades/Atividade01/tempo.c
@@ -5,33 +5,39 @@
 int main(){
 
     int opcao;
-    int valor;
+    float valor;
     float x;
 
-    printf("Qual o valor a ser convertido: ");
-    scanf("%d", &valor);
+    printf("Qual o valor (em segundos) a ser convertido: ");
+    scanf("%f", &valor);
     getchar();
 
     printf("Conversao segundos x horas: 1\n");
-    printf("Conversao horas x segundos: 2\n");
+    printf("Conversao segundos x minutos: 2\n");
+    printf("Conversao segundos x segundos: 3\n");
     printf("Digite sua opcao: ");
     scanf("%d", &opcao);
     getchar();
 
     if(opcao == 1){
         
-        x = valor % 3600;
+        x = valor / 3600;
         printf("O numero em horas eh: %.4f\n", x);
 
     } else if(opcao == 2){
 
         x = valor / 60;
-        printf("O numero em segudos eh: %f\n", x);
+        printf("O numero em minutos eh: %f\n", x);
 
     } else if(opcao == 3) {
 
          x = valor * 3600;
-        printf("O numero em segundos eh: %d\n", x);
+        printf("O numero em segundos eh: %f\n", x);
+        
+    } else {
+
+        printf("Essa opcao eh invalida");
+        
     }
 
     return 0;
