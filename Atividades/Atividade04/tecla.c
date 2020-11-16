742f2f5db020c04ede8afcd688e4530a587710ec
Pedro Mota <pedroheriquecarvalhomota@gmail.com>
2020-11-16 15:21:27 -0300

    Letra, numero ou caractere

--- Atividades/Atividade04/tecla.c
+++ Atividades/Atividade04/tecla.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
+#include <ctype.h>
+
+// A-Z ou a-z = letra
+// 0-9 = digito
+// else = caractere especial
 
 int main(){
     
-    int tecla;
+    char tecla;
+
+    printf("Digite uma tecla: ");
+    scanf("%c", &tecla);
+    getchar();
+
+    if(isalpha(tecla))
+    {
+        printf("%c eh uma letra\n", tecla);
+    }
+    if(isdigit(tecla))
+    {
+        printf("%c eh um digito\n", tecla);
+    }
+    else if(isdigit(tecla)==0 && isalpha(tecla)==0){
+        printf("%c eh um caractere especial\n", tecla);
+    }
 
     return 0;
 }
